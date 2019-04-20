3a6d1caf2ea8a00565785ce24728410b3a103c6a
Anton Sadovnikov <sadovnikov.a@concord-consulting.org>
2019-04-20 14:14:50 +0300

    init test project

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	"github.com/valyala/fasthttp"
+)
+
+func handler(ctx *fasthttp.RequestCtx) {
+	ctx.SetStatusCode(200)
+	ctx.SetBody([]byte("ok"))
+}
+
+type fasthttpLogger struct{}
+
+func (f fasthttpLogger) Printf(format string, args ...interface{}) {
+	log.Printf(format, args...)
+}
+
+func main() {
+	server := &fasthttp.Server{
+		Logger:  &fasthttpLogger{},
+		Handler: handler,
+		ErrorHandler: func(ctx *fasthttp.RequestCtx, err error) {
+			log.Printf("%v", err)
+		},
+		Concurrency:          256 * 1024,
+		DisableKeepalive:     false,
+		ReadTimeout:          10 * time.Second,
+		WriteTimeout:         10 * time.Second,
+		ReadBufferSize:       4096,
+		WriteBufferSize:      4096,
+		MaxConnsPerIP:        256,
+		MaxRequestsPerConn:   0,
+		MaxKeepaliveDuration: 30 * time.Second,
+		MaxRequestBodySize:   4 * 1024 * 1024,
+		//
+		LogAllErrors:                  false,
+		DisableHeaderNamesNormalizing: false,
+	}
+
+	panic(server.ListenAndServe(":8080"))
+}
